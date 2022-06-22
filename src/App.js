ee5c42cbba5c00c5f001b0cd32c7d81f621b1b56
Magdalena Kierat <m.kierat@gmx.de>
2022-06-22 11:27:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Komponenten müssen mit einem Großbuchstaben anfangen
+const HelloMuc = () => {
+  return <p>Hello Muc-3</p>;
+};
+
+const HelloHeader = () => {
+  return <h1>Ich bin eine H1</h1>;
+};
+
+const HelloFooter = () => {
+  return (
+    <footer className="app__footer">
+      <ul>
+        <li>Link1</li>
+        <li>Link2</li>
+        <li>Link3</li>
+      </ul>
+    </footer>
+  );
+};
 
 function App() {
+  // ab hier JSX
   return (
     <div className="App">
       <header className="App-header">
+        <HelloHeader />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HelloMuc /> {/* Hauptcontainer */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +39,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HelloFooter />
     </div>
   );
 }
